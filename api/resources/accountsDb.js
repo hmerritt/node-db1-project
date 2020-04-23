09ecd98a7b61c90c93a81701a0913ae4520dfa21
Harry Merritt <harry@merritt.es>
2020-04-23 02:52:40 +0100

    Delete an account

--- api/resources/accountsDb.js
+++ api/resources/accountsDb.js
@@ -1,16 +1,24 @@
 const db = require("../../data/dbConfig");
 
 
+//
 function get(id=null) {
     if (id)
     {
-        return db("accounts").where("name", `account-${id}`).first();
+        //  Get individial account
+        return db("accounts").where("id", id).first();
     }
     else
     {
+        //  Get all accounts
         return db("accounts");
     }
 }
 
+//  Delete account
+function remove(id) {
+    return db("accounts").where("id", id).del();
+}
 
-module.exports = {get};
+
+module.exports = {get, remove};
--- api/resources/accountsRouter.js
+++ api/resources/accountsRouter.js
@@ -14,11 +14,30 @@ router.get("/", async (req, res, next) => {
     }
 });
 
-//  Get all accounts
+//  Get individial account
 router.get("/:id", async (req, res, next) => {
     try {
         const accounts = await db.get(req.params.id);
         res.send(accounts);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+//  Delete an account
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const account = await db.remove(req.params.id);
+        if (account) {
+            res.json({
+                message: `Account {${req.params.id}} deleted successfully`
+            });
+        }
+        else
+        {
+            next(err);
+        }
     }
     catch (err) {
         next(err);
